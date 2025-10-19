ba593c0e3de9c775e441529f7ba5fa480aa518da
Sandile Sibiya <165039945+SandileSibiya@users.noreply.github.com>
2025-10-19 20:47:40 +0200

    Delete Downloads/UniversityDigitalCard (2)/UniversityDigitalCard directory
    It doesn't match with the rest of the code, wrong directory

--- Downloads/UniversityDigitalCard (2)/UniversityDigitalCard/src/main/java/za/ac/cputdigitalcard/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cputdigitalcard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cputdigitalcard.domain.Card;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-    Card findByCardId(Long cardId);
-    Card findByStudentId(Long studentId);
-}
--- Downloads/UniversityDigitalCard (2)/UniversityDigitalCard/src/main/java/za/ac/cputdigitalcard/service/IService.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cputdigitalcard.service;
-
-public interface IService <T , ID >{
-    T create(T t);
-    T read(ID id);
-    T update(T t);
-    T delete(ID id);
-
-}
